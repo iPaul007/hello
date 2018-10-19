ff55a03161806aac5c7c2fb30fccbdf0e8073211
iPaul007 <1218059079@qq.com>
2018-10-19 15:08:18 +0800

    first

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: helloworld.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年10月19日 星期五 15时06分59秒
+ ************************************************************************/
+
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    
+    }
+
+
+}
+
